4b4d934fae76dae214a218a531a8d4554c11a4a2
MarkPreschern <mark.preschern@gmail.com>
2020-09-12 21:53:14 -0400

    completed assignment 'B - Command Line'

---
+++ B/other/Xyes.java
@@ -0,0 +1,31 @@
+package other;
+
+/*
+ * a shell-executable command-line program that echos the command line arguments to standard output
+ */
+public class Xyes {
+    /*
+     * Accepts command line arguments to print
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        String str = "hello world";
+        boolean limit = false;
+
+        if (args.length > 0) {
+            if (args[0].equals("-limit")) {
+                limit = true;
+                str = String.join(" ", args).substring(args[0].length()).trim(); // removes "-limit" from str
+            } else {
+                str = String.join(" ", args).trim();
+            }
+        }
+
+        int counter = 0;
+        while (!limit || counter < 20) {
+            System.out.println(str);
+            counter++;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # texline
+
+Mark Preschern and Joseph Mullally's CS4500 assignments.
+
+## B - Command Line
+
+xyes is a shell-executable command-line program that echos the command line arguments to standard
+output. The purpose is to to get to know our partners with a tiny on-line assignment and to learn
+to live up very rudimentary specifications.
+
+
+
+
