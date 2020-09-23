ea30167bb87ad0e257c39d8a7628094fe2da14df
Joseph Mullally <jd.mullally@gmail.com>
2020-09-22 22:02:39 -0400

    fixed newline errors and null value errors

--- C/Other/src/Xjson.java
+++ C/Other/src/Xjson.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
@@ -32,37 +33,44 @@ public class Xjson {
 
         int count = 0;
         JsonArray array = new JsonArray();
-        while (scanner.hasNext()) {
-            String s = scanner.next();
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
 
-            if (s.charAt(0) == '{' || s.charAt(0) == '[') {
+            if(!s.isEmpty()) {
+                // parse json
                 JsonReader reader = new JsonReader(new StringReader(s));
                 reader.setLenient(true);
-                array.add(JsonParser.parseReader(reader));
-                count++;
-            }
-
-            // parse json
-            JsonReader reader = new JsonReader(new StringReader(s));
-            reader.setLenient(true);
-            try {
-                while (reader.hasNext()) {
-                    if (JsonToken.NUMBER.equals(reader.peek())) {
-                        array.add(reader.nextLong());
-                        count++;
-                    } else if (JsonToken.BOOLEAN.equals(reader.peek())) {
-                        array.add(reader.nextBoolean());
-                        count++;
-                    } else if (JsonToken.STRING.equals(reader.peek())) {
-                        array.add(reader.nextString());
-                        count++;
-                    } else {
-                        break;
+                try {
+                    while (reader.hasNext()) {
+                        JsonToken nextToken = reader.peek();
+                        if (JsonToken.NUMBER.equals(nextToken)) {
+                            array.add(reader.nextLong());
+                            count++;
+                        } else if (JsonToken.BOOLEAN.equals(nextToken)) {
+                            array.add(reader.nextBoolean());
+                            count++;
+                        } else if (JsonToken.STRING.equals(nextToken)) {
+                            array.add(reader.nextString());
+                            count++;
+                        } else if (JsonToken.NULL.equals(nextToken)) {
+                            count++;
+                            array.add("NULL"); // Can't add a null value to JsonArray, but can add string null
+                            reader.nextNull();
+                        } else if (JsonToken.BEGIN_OBJECT.equals(nextToken)) {
+                            JsonObject built = buildObj(reader);
+                            count++;
+                            array.add(built);
+                        } else if (JsonToken.BEGIN_ARRAY.equals(nextToken)) {
+                            JsonArray built = buildArr(reader);
+                            count++;
+                            array.add(built);
+                        } else {
+                            break;
+                        }
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
             }
         }
         scanner.close();
@@ -80,5 +88,79 @@ public class Xjson {
             jsonArray.add(array.get(i));
         }
         System.out.println(jsonArray.toString());
+    }
+
+    public static JsonObject buildObj(JsonReader reader) {
+        JsonObject build = new JsonObject();
+        try {
+            while (reader.hasNext()) {
+                if (JsonToken.BEGIN_OBJECT.equals(reader.peek())) {
+                    reader.beginObject();
+                } else if (JsonToken.NAME.equals(reader.peek())) {
+                    String n = reader.nextName();
+                    if (JsonToken.BOOLEAN.equals(reader.peek())) {
+                        Boolean b = reader.nextBoolean();
+                        build.addProperty(n, b);
+                    } else if (JsonToken.NUMBER.equals(reader.peek())) {
+                        Integer i = reader.nextInt();
+                        build.addProperty(n, i);
+                    } else if (JsonToken.STRING.equals(reader.peek())) {
+                        String s = reader.nextString();
+                        build.addProperty(n, s);
+                    } else if (JsonToken.BEGIN_OBJECT.equals(reader.peek())) {
+                        JsonElement obj = buildObj(reader);
+                        build.add(n,obj);
+                    } else if (JsonToken.BEGIN_ARRAY.equals(reader.peek())) {
+                        JsonElement arr = buildArr(reader);
+                        build.add(n, arr);
+                    }
+                } else if (JsonToken.END_OBJECT.equals(reader.peek())) {
+                    reader.endObject();
+                    return build;
+                } else {
+                    break;
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return build;
+    }
+
+    public static JsonArray buildArr(JsonReader reader) throws IOException {
+        JsonArray build = new JsonArray();
+        try {
+            while (reader.hasNext()) {
+                if (JsonToken.BEGIN_ARRAY.equals(reader.peek())) {
+                    reader.beginArray();
+                }
+                else if (JsonToken.BOOLEAN.equals(reader.peek())) {
+                    Boolean b = reader.nextBoolean();
+                    build.add(b);
+                } else if (JsonToken.NUMBER.equals(reader.peek())) {
+                    Integer i = reader.nextInt();
+                    build.add(i);
+                } else if (JsonToken.STRING.equals(reader.peek())) {
+                    String s = reader.nextString();
+                    build.add(s);
+                } else if (JsonToken.BEGIN_OBJECT.equals(reader.peek())) {
+                    JsonElement obj = buildObj(reader);
+                    build.add(obj);
+                } else if (JsonToken.BEGIN_ARRAY.equals(reader.peek())) {
+                    JsonElement arr = buildArr(reader);
+                    build.add(arr);
+                }
+                else if (JsonToken.END_ARRAY.equals(reader.peek())) {
+                    reader.endArray();
+                    return build;
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Oops");
+        }
+        return build;
     }
 }
--- C/Other/test.sh
+++ C/Other/test.sh
@@ -5,6 +5,4 @@ if ! test -f "$FILE"; then
     echo "$FILE does not exist."
 fi
 
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'args=($(cat ../Test/1-in.json))'
-
-java -jar ../Other/xjson.jar "${args[@]}" > ../Test/1-out.json
+java -jar ../../xjson.jar < "../Test/1-in.json" > "../Test/1-out.json"
