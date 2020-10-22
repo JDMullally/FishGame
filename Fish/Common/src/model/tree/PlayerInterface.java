5105acf3b410a1b240790873b628c5441f897b64
MarkPreschern <mark.preschern@gmail.com>
2020-10-21 23:42:08 -0400

    Updated PlayerInterface, player-protocal, README.md, and 3/xboard

--- Fish/Common/src/model/tree/PlayerInterface.java
+++ Fish/Common/src/model/tree/PlayerInterface.java
@@ -15,69 +15,100 @@ import model.state.IPlayer;
 public interface PlayerInterface {
 
     /**
-     * Allows the Player to queue up the placement of their penguin.
-     * If they placed the penguin in an invalid spot, throw an Illegal Argument Exception.
-     * @param gameState
-     * @param player
-     * @param penguin
-     * @param point
-     * @return IPenguin that the Player Placed.
-     * @throws IllegalArgumentException
+     * Returns a copy of the current GameState.
+     *
+     * @return IGameState representing the current game state
      */
-    IPenguin queuePlacePenguin(IGameState gameState, IPlayer player,
-                               IPenguin penguin, Point point) throws IllegalArgumentException;
+    IGameState getGameState();
+
+    /**
+     * Returns a copy of a complete game tree. Every time the method is called again, a new subtree
+     * for the bottom most node(s) in the tree will be created. i.e. the resulting IGameTree that
+     * is returned will have 1 more depth to it.
+     *
+     * @return IGameTree representing a complete game tree
+     */
+    IGameTree getGameTree();
 
     /**
      * Returns 0 if it is the given Player's turn, or an integer between 1 and the number of
-     * Players in the current IGame game.  This other number lets the player know their placement
-     * in the queue.
-     * @param player
-     * @param game
-     * @return int
+     * Players in the current game minus one. This other number lets the player know their placement
+     * in the queue (how many turns until it is their turn).
+     *
+     * @param player IPlayer's turn
+     * @return int representing the player's placement in the queue of turns
      */
-    int myTurn(IPlayer player, IGameTree game);
+    int getTurn(IPlayer player);
 
     /**
-     * Returns a Map of Points to GameStates that allows players to see the states the moving to
-     * that point with the input Penguin would yield.  This would work as the building blocks for
-     * a tree of potential moves for an AI Player.
-     * @param game
-     * @param player
-     * @param penguin
+     * Returns the score a given player has accumulated across all of their penguins in the game.
+     *
+     * @param player IPlayer who's checking their score
+     * @return the player's score as an int
+     */
+    int getPlayerScore(IPlayer player);
+
+    /**
+     * Returns a Map of potential Points to move to mapped to the resulting IGameState of that move.
+     *
+     * @param game IGameState being moved on
+     * @param player IPlayer playing moving
+     * @param penguin IPenguin moving
      * @return Map of Point to IGameState
      */
-    Map<Point,IGameState> possibleMoves(IGameTree game, IPlayer player, IPenguin penguin);
+    Map<Point, IGameState> getPossibleMoves(IGameState game, IPlayer player, IPenguin penguin);
 
     /**
-     * Allows a Player to queue a move to be played on their turn.
-     * If the move is invalid, throw an error.  This is the primary way a
-     * Player will interact with the Game.
-     * @param game
-     * @param player
-     * @param penguin
-     * @param newPoint
-     * @throws IllegalArgumentException
+     * Allows the Player to add themself to the game. If the game already has 4 players, an error
+     * will be thrown.
+     *
+     * @param player IPlayer being added to the game
+     * @return IPlayer that is added to the game
+     * @throws IllegalArgumentException if the player cannot be added to the game (if there is
+     * already 4 players in the game)
      */
-    void queueMove(IGameTree game, IPlayer player,
-        IPenguin penguin, Point newPoint) throws IllegalArgumentException;
+    IPlayer addPlayer(IPlayer player) throws IllegalArgumentException;
 
     /**
-     * Allows a Player to check whether the game is over.
-     * @param game
-     * @return
+     * Allows the Player to queue up the placement of their penguin/ If they attempt to place the
+     * penguin in an invalid spot, an Illegal Argument Exception will be thrown.
+     *
+     * A penguin placement is invalid for a given point if:
+     * - The point is outside the bounds of the board
+     * - The point is a hole on the board
+     * - The point is already occupied by another penguin
+     * - The player has already placed their maximum number of penguins
+     *
+     * @param player IPlayer placing penguin
+     * @param penguin IPenguin being placed
+     * @param point Point to place penguin
+     * @return IPenguin that the Player Placed
+     * @throws IllegalArgumentException if the penguin cannot be placed
      */
-    boolean isGameOver(IGameTree game);
+    IPenguin queuePlacePenguin(IPlayer player, IPenguin penguin, Point point) throws IllegalArgumentException;
 
     /**
-     * Returns the points they have gathered across all of their penguins in this game.
-     * @param gameState
-     * @return int
+     * Allows a Player to queue a move to be played on their turn. If the move is invalid, throw an
+     * error. This is the primary way a Player will interact with the Game.
+     *
+     * A move is invalid for a given point if:
+     * - The point is outside the bounds of the board
+     * - The point is a hole on the board
+     * - The point is already occupied by another penguin
+     * - The point is not reachable from the penguin's current position
+     *
+     * @param player IPlayer playing moves
+     * @param penguin IPenguin moving
+     * @param newPoint Point to move to
+     * @throws IllegalArgumentException If the move is invalid
      */
-    int playerPoints(IGameState gameState);
+    void queueMove(IPlayer player, IPenguin penguin, Point newPoint) throws IllegalArgumentException;
 
     /**
-     * Gets a copy of the current GameState
-     * @return IGameState
+     * Allows a Player to check whether the game is over. A game is over if no player in the game
+     * is able to make a move.
+     *
+     * @return True if the game is over and False otherwise
      */
-    IGameState getCurrentGameState();
+    boolean isGameOver();
 }
--- Fish/Planning/player-protocol.md
+++ Fish/Planning/player-protocol.md
@@ -8,18 +8,28 @@
 
 `Date:` 10/20/2020
 
-A player should be able to see all possible games, get a specific game by its ID,
-create a player in a game, get all the players in a current game,
-get a specific player from a specific game, look at their own Penguins, get a specific Penguin,
-get all possible moves with a specific penguin and make a move with their penguin.
+A player should be able to do the following in order to interact properly with our API:
 
-This implies the a User has a one to many relationship between them and Games, 
-a Game has a one(two) to many relationship with players, a player has a one to many
-relationship with their penguins and penguin has a one to many relationship with it's moves.
+- Get All Current Games
+- Get a Game (GameState) by ID 
+- Get a Game's GameTree
+- Get if a Game is over
+- Get All Players in a Game
+- Get a Player in a Game by ID
+- Get a Player's turn in a Game
+- Get a Player's score in a Game
+- Get all of a Player's Penguins in a Game
+- Get all of a Player's possible moves with a Penguins in a Game
+- add a new Player in a Game
+- add a Penguin for a Player in a Game
+- move a Penguin for a Player in a Game
+
+This implies the a User has a one to many relationship between them and Games, a Game has a one to
+many relationship with players, and a player has a one to many relationship with their penguins.
 
 We decided that best protocol for our API would be a RESTFUL one.
 
-The following is an example of the request methods for a player.
+The following is an example of the request methods for a player to use.
 
 #### Request Methods
 
@@ -27,37 +37,50 @@ To Get All Current Games:
 
 `GET /games`
 
-To Get a specific Game by ID: 
+To Get a Game (GameState) by ID: 
 
 `GET /games/{id}`
 
-To Create a new Player:
+To Get a Game's GameTree:
 
-`POST /games/{id}/players`
+`GET /games/{id}/tree`
 
-To Get All Players in a specific Game:
+To Get if a Game is over:
+
+`GET /games/{id}/gameover`
+
+To Get All Players in a Game:
 
 `GET /games/{id}/players`
 
-To Get a specific Player in a specific game by ID:
+To Get a Player in a Game by ID:
 
 `GET /games/{id}/players/{id}`
 
-To Get a specific Player's Penguins:
+To Get a Player's turn in a Game:
+
+`GET /games/{id}/players/{id}/turn`
+
+To Get a Player's score in a Game:
+
+`GET /games/{id}/players/{id}/score`
+
+To Get all of a Player's Penguins in a Game:
 
 `GET /games/{id}/players/{id}/penguins`
 
-To Get a Penguin by ID:
-
-`GET /games/{id}/players/{id}/penguins/{id}`
-
-To Get all moves with a specific Penguin
+To Get all of a Player's possible moves with a Penguins in a Game:
 
 `GET /games/{id}/players/{id}/penguins/{id}/moves`
 
-To Make a move
+To add a new Player in a Game:
 
-`PUT /games/{id}/players/{id}/penguins/{id}/moves/{id}`
+`POST /games/{id}/players`
 
-####Request Protocols
+To add a Penguin for a Player in a Game:
 
+`POST /games/{id}/players/{id}/penguins/{id}`
+
+To move a Penguin for a Player in a Game:
+
+`POST /games/{id}/players/{id}/penguins/{id}/move`
