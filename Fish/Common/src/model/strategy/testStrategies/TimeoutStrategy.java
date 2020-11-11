0a628252dd332735e93267bfa519dfaba375001b
Joseph Mullally <jd.mullally@gmail.com>
2020-11-10 19:07:27 -0500

    Created new Timeout Strategy for testing

--- Fish/Common/src/model/strategy/testStrategies/TimeoutStrategy.java
+++ Fish/Common/src/model/strategy/testStrategies/TimeoutStrategy.java
@@ -1,5 +1,33 @@
 package model.strategy.testStrategies;
 
-public class TimeoutStrategy {
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import model.state.IGameState;
+import model.strategy.IStrategy;
+import model.strategy.Strategy;
+import model.tree.Action;
+import model.tree.PlayerInterface;
 
+public class TimeoutStrategy implements IStrategy {
+
+    private IStrategy strategy;
+
+    public TimeoutStrategy() {
+        this.strategy = new Strategy();
+    }
+
+    @Override
+    public Action choosePlacePenguinAction(IGameState state) {
+        try {
+            TimeUnit.MINUTES.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return this.strategy.choosePlacePenguinAction(state);
+    }
+
+    @Override
+    public Action chooseMoveAction(IGameState state, int turns) {
+        return this.strategy.choosePlacePenguinAction(state);
+    }
 }
