b5687bc94f2d77252ace450fddd2615f5fb15652
Joseph Mullally <jd.mullally@gmail.com>
2020-11-15 21:12:45 -0500

    Testing task code

--- Fish/Common/src/util/RefereeTestUtil.java
+++ Fish/Common/src/util/RefereeTestUtil.java
@@ -1,5 +1,82 @@
 package util;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonStreamParser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import model.games.IGameResult;
+import model.games.IReferee;
+import model.games.PlayerAI;
+import model.games.Referee;
+import model.strategy.IStrategy;
+import model.strategy.Strategy;
+import model.tree.PlayerInterface;
+
 public class RefereeTestUtil {
 
+    public static void main(String[] args) throws IOException {
+        test();
+    }
+
+    private static void test() throws IOException {
+        Gson gson = new Gson();
+        InputStreamReader reader = new InputStreamReader(System.in);
+        JsonStreamParser parser = new JsonStreamParser(reader);
+        JsonObject element;
+
+        if (parser.hasNext()) {
+            element = parser.next().getAsJsonObject();
+        } else {
+            return;
+        }
+
+        int row = element.get("row").getAsInt();
+        int col = element.get("column").getAsInt();
+        int fish = element.get("fish").getAsInt();
+
+        JsonArray players = element.get("players").getAsJsonArray();
+
+        LinkedHashMap<PlayerInterface, String> interfacePlayers
+            = jsonPlayersToInterfacePlayers(players);
+
+        List<PlayerInterface> gamePlayers = new ArrayList<>(interfacePlayers.keySet());
+
+        IReferee referee = new Referee(gamePlayers, row, col, fish);
+        referee.runGame();
+
+        IGameResult result = referee.getGameResult();
+
+        List<PlayerInterface> playerPlacements = result.getWinners();
+
+        JsonArray winners = new JsonArray();
+
+        for (PlayerInterface p : playerPlacements) {
+            winners.add(interfacePlayers.get(p));
+        }
+        System.out.println(winners);
+
+        reader.close();
+
+    }
+
+    private static LinkedHashMap<PlayerInterface, String> jsonPlayersToInterfacePlayers(JsonArray players) {
+        LinkedHashMap<PlayerInterface, String> map = new LinkedHashMap<>();
+        for (JsonElement player: players) {
+            String name = player.getAsJsonArray().get(0).getAsString();
+            int depth = player.getAsJsonArray().get(1).getAsInt();
+
+            IStrategy strategy = new Strategy();
+            PlayerInterface newPlayer = new PlayerAI(strategy, depth);
+
+            map.put(newPlayer, name);
+        }
+        return map;
+    }
 }
