9fc24c32a5b1c65a21cbc5c32ee2c75e94c0b2c1
Joseph Mullally <jd.mullally@gmail.com>
2020-11-16 16:07:28 -0500

    added testing files for PlayerAI

---
+++ Fish/Common/test/model/PlayerAITest.java
@@ -0,0 +1,21 @@
+package model;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+public class PlayerAITest {
+
+
+
+    @Test
+    public void getPlayerAge() {
+    }
+
+    @Test
+    public void getPlayerID() {
+    }
+}
