ec371ff2f49e9872ea8fdc3232a2959acf366b0d
Joseph Mullally <jd.mullally@gmail.com>
2020-11-16 17:22:41 -0500

    added enum for a player's standing in a game

--- Fish/Common/src/model/tournament/PlayerStanding.java
+++ Fish/Common/src/model/tournament/PlayerStanding.java
@@ -1,4 +1,11 @@
 package model.tournament;
 
+/**
+ * Represents a Player's current standing in a tournament.  They were are either caught cheating,
+ * eliminated, or are still playing.
+ */
 public enum PlayerStanding {
+    CHEATER,
+    ELIMINATED,
+    REMAINING
 }
