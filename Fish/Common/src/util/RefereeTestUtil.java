10429ead737640a54bb6dc8ad34bc888abef9ab2
Joseph Mullally <jd.mullally@gmail.com>
2020-11-18 16:04:26 -0500

    Cleaned up imports for Referee testing task

--- Fish/Common/src/util/RefereeTestUtil.java
+++ Fish/Common/src/util/RefereeTestUtil.java
@@ -2,32 +2,21 @@ package util;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonStreamParser;
-import controller.Controller;
-import java.awt.JobAttributes;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import model.games.GameAction;
 import model.games.IGameResult;
 import model.games.IReferee;
 import model.games.PlayerAI;
 import model.games.Referee;
-import model.state.IPlayer;
-import model.state.ImmutableGameState;
-import model.state.ImmutableGameStateModel;
-import model.state.Player;
 import model.strategy.IStrategy;
 import model.strategy.Strategy;
 import model.tree.PlayerInterface;
-import view.IView;
-import view.VisualView;
 
 public class RefereeTestUtil {
 
