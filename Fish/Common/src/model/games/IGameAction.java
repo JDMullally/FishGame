75944525c0b2b5dc4f59685b5f1b74a96c2fca8c
Joseph Mullally <jd.mullally@gmail.com>
2020-11-24 22:58:55 -0500

    added interface for GameAction after it was given a public method

--- Fish/Common/src/model/games/IGameAction.java
+++ Fish/Common/src/model/games/IGameAction.java
@@ -1,5 +1,20 @@
 package model.games;
 
+import model.state.ImmutableGameStateModel;
+
+/**
+ * Interface for an IGameAction.  A Game action is a visual representation of an Action. It acts as
+ * an Immutable action that can only be used on ImmutableGameStateModels to get the next
+ * ImmutableGameStateModel.
+ */
 public interface IGameAction {
+
+    /**
+     * Gets the next ImmutableGameStateModel based on the current Action the GameAction holds and the
+     * given ImmutableGameStateModel
+     * @param model
+     * @return
+     */
+    ImmutableGameStateModel getNextState(ImmutableGameStateModel model);
 
 }
