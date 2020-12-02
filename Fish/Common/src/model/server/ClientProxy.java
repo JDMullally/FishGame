b75b299af95e57ebe8a98640a97cf38002a6493b
Joseph Mullally <jd.mullally@gmail.com>
2020-12-01 23:59:08 -0500

    completed stdin and stdout calls for proxy players

--- Fish/Common/src/model/server/ClientProxy.java
+++ Fish/Common/src/model/server/ClientProxy.java
@@ -1,74 +1,203 @@
 package model.server;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonStreamParser;
 import java.awt.Color;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 import model.games.IGameResult;
 import model.state.IGameState;
 import model.tree.Action;
 import model.tree.PlayerInterface;
+import constants.Constants;
+import util.ColorUtil;
+import util.GameStateUtil;
+import util.PlayerUtil;
 
 public class ClientProxy implements PlayerInterface {
 
-    private Client client;
+    private Socket socket;
+    private final DataInputStream dis;
+    private final DataOutputStream dos;
+    private JsonStreamParser jsonStreamParser;
+    private final int age;
+    private String name;
+    private final PlayerUtil util = new PlayerUtil();
+    private List<Action> observedActions;
 
-    public ClientProxy(Client client, int age, String id) {
-        if (client == null || id == null ||id.length() > 12) {
+    public ClientProxy(Socket s, DataInputStream dis, DataOutputStream dos, int age) {
+        if (s == null || dis == null || dos == null || age < 1) {
             throw new IllegalArgumentException("Invalid ClientProxy Player");
         }
-        this.client = client;
+        this.socket = s;
+        this.dis = dis;
+        this.dos = dos;
+        this.age = age;
+        InputStreamReader reader = new InputStreamReader(dis);
+        this.jsonStreamParser = new JsonStreamParser(reader);
+        this.name = this.getName();
+        this.observedActions = new ArrayList<>();
     }
+
+    public void addObservedAction(Action action) {
+        this.observedActions.add(action);
+    }
+
+    public void resetActions() {
+        this.observedActions = new ArrayList<>();
+    }
+
+    private String getName() throws IllegalArgumentException {
+        String newName = "";
+        try {
+            newName = dis.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (name.length() == 0 || name.length() > 12) {
+            throw new IllegalArgumentException("This name is not the right length");
+        }
+        return newName;
+    }
+
 
     @Override
     public Action placePenguin(IGameState state) throws TimeoutException {
-        return this.client.setUp(state);
+        try {
+            JsonArray function = new JsonArray();
+            function.add(Constants.takeTurn);
+            JsonObject jsonState = this.util.GameStateToJson(state);
+            JsonArray parameters = new JsonArray();
+            parameters.add(jsonState);
+            function.add(parameters);
+
+            this.dos.writeUTF(function.toString());
+
+            if(this.jsonStreamParser.hasNext()) {
+                JsonElement element = this.jsonStreamParser.next();
+                return util.toPlacementAction(element, state);
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return null;
     }
+
 
     @Override
     public Action movePenguin(IGameState state) throws TimeoutException {
-        return this.client.takeTurn(state);
+        try {
+            JsonArray function = new JsonArray();
+            function.add(Constants.takeTurn);
+            JsonObject jsonState = this.util.GameStateToJson(state);
+            JsonArray parameters = new JsonArray();
+            parameters.add(jsonState);
+            parameters.add(this.util.GameActionsToJson(this.observedActions));
+            function.add(parameters);
+
+            this.dos.writeUTF(function.toString());
+
+            if(this.jsonStreamParser.hasNext()) {
+                JsonElement element = this.jsonStreamParser.next();
+                return util.toPlacementAction(element, state);
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return null;
     }
 
     @Override
     public int getPlayerAge() {
-        return this.client.getAge();
+        return this.age;
     }
 
     @Override
     public String getPlayerID() {
-        return this.client.getID();
+        return this.name;
     }
 
+    //TODO
     @Override
     public boolean tournamentHasStarted() {
-        this.client.start(true);
+        try {
+            JsonArray function = new JsonArray();
+            function.add(Constants.startTournament);
+            JsonArray parameters = new JsonArray();
+            parameters.add(true);
+            function.add(parameters);
+            this.dos.writeUTF(function.toString());
+        } catch (IOException e) {
+            return false;
+        }
         return true;
     }
 
+    //Not necessary for remote interactions
     @Override
     public boolean gameHasStarted() {
         return true;
     }
 
+    //Not necessary for remote interactions
     @Override
     public void kickedForCheating() { }
 
+    //Not necessary for remote interactions
     @Override
     public void gameResults(IGameResult result) { }
 
     @Override
     public boolean tournamentResults(boolean youWon) {
-        this.client.end(youWon);
+        try {
+            JsonArray function = new JsonArray();
+            function.add(Constants.end);
+            JsonArray parameters = new JsonArray();
+            parameters.add(youWon);
+            function.add(parameters);
+            this.dos.writeUTF(function.toString());
+        } catch (IOException e) {
+            return false;
+        }
         return true;
     }
 
     @Override
     public void playerColor(Color color) {
-        this.client.playingAs(color);
+        try {
+            JsonArray function = new JsonArray();
+            function.add(Constants.playAs);
+            JsonArray parameters = new JsonArray();
+            parameters.add(ColorUtil.toColorString(color));
+            function.add(parameters);
+            this.dos.writeUTF(function.toString());
+        } catch (IOException e) { }
     }
 
     @Override
     public void otherPlayerColors(List<Color> otherPlayers) {
-        this.client.playingWith(otherPlayers);
+        try {
+            JsonArray function = new JsonArray();
+            function.add(Constants.playAs);
+            JsonArray parameters = new JsonArray();
+            for (Color color : otherPlayers) {
+                parameters.add(ColorUtil.toColorString(color));
+            }
+            function.add(parameters);
+            this.dos.writeUTF(function.toString());
+        } catch (IOException e) { }
+    }
+
+    @Override
+    public void getOnGoingAction(Action action) {
+
     }
 }
