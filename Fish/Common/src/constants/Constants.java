1a80d241045d28d680fdada0a7067b5dd3f76297
Joseph Mullally <jd.mullally@gmail.com>
2020-12-01 23:54:13 -0500

    added constants for Json String methods

--- Fish/Common/src/constants/Constants.java
+++ Fish/Common/src/constants/Constants.java
@@ -6,4 +6,16 @@ package constants;
 public final class Constants {
     // hexagon width = 3 * hexSize, hexagon height = 2 * hexSize
     public final static int HEX_SIZE = 30;
+
+    public final static String end = "end";
+
+    public final static String playAs = "play-as";
+
+    public final static String startTournament = "start";
+
+    public final static String playWith = "play-with";
+
+    public final static String setup = "setup";
+
+    public final static String takeTurn = "take-turn";
 }
