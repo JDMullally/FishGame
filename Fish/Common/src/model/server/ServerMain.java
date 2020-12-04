a28cb1a020fb58a5359bf6474597a979eb680155
Joseph Mullally <jd.mullally@gmail.com>
2020-12-04 02:14:25 -0500

    made a main for server runnable

--- Fish/Common/src/model/server/ServerMain.java
+++ Fish/Common/src/model/server/ServerMain.java
@@ -1,5 +1,20 @@
 package model.server;
 
-public class Server {
+import java.io.IOException;
+
+public class ServerMain {
+
+    public static void main(String[] args) throws IOException {
+        int port = -1;
+        try {
+            port = Integer.getInteger(args[0]);
+        } catch (Exception e) {
+            System.out.println("usage: ./xserver <port number>");
+            System.exit(0);
+        }
+        Server server = new SignUpServer(port);
+        server.signUp();
+        server.sendToTournamentManager();
+    }
 
 }
