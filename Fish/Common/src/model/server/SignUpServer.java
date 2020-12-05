dccee485c0882b95495e49f53e3a59e40fe7986b
Joseph Mullally <jd.mullally@gmail.com>
2020-12-04 22:50:28 -0500

    changed signUp to acccept 6 players no matter what instead of being abroken mess

--- Fish/Common/src/model/server/SignUpServer.java
+++ Fish/Common/src/model/server/SignUpServer.java
@@ -1,14 +1,13 @@
 package model.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
-import model.games.IGameResult;
 import model.tournament.ManagerInterface;
 import model.tournament.PlayerStanding;
 import model.tournament.TournamentManager;
@@ -17,7 +16,7 @@ import model.tree.PlayerInterface;
 public class SignUpServer implements Server {
 
     private static final int PORT = 4567;
-    private final int  MAX_PLAYERS = 10;
+    private final int MAX_PLAYERS = 10;
     private final int MIN_PLAYERS = 5;
     private final int timeout = 30000;
     private final int infiniteTimout = 0;
@@ -27,45 +26,29 @@ public class SignUpServer implements Server {
     public SignUpServer(int port) throws IOException {
         this.proxyPlayerList = new ArrayList<>();
         this.server = new ServerSocket(port);
-        this.signUp();
-        List<PlayerInterface> result = this.sendToTournamentManager();
     }
 
     @Override
-    public void signUp() throws IOException {
-        int count = 0;
-        int repeat = 0;
-        while (repeat < 2) {
-            this.server.setSoTimeout(this.timeout);
-            try {
-                if (proxyPlayerList.size() < MAX_PLAYERS) {
-                    Socket s = this.server.accept();
-                    DataInputStream dis = new DataInputStream(s.getInputStream());
-                    DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-                    PlayerInterface clientProxy = new ClientProxy(s, dis, dos, count + 1);
-                    count++;
-                    this.proxyPlayerList.add(clientProxy);
-                } else {
-                    break;
-                }
-            } catch (SocketTimeoutException e) {
-                if (repeat > 0 && proxyPlayerList.size() < MIN_PLAYERS) {
-                    System.out.println("Not enough players connected");
-                    return;
-                }
-            }
-            repeat++;
+    public List<PlayerInterface> signUp() throws IOException {
+        for (int i = 0; i < 6; i++) {
+        Socket s = this.server.accept();
+        PlayerInterface clientProxy = new ClientProxy(s, i + 1);
+        this.proxyPlayerList.add(clientProxy);
         }
-        //this.server.setSoTimeout(this.infiniteTimout);
+        return new ArrayList<>(this.proxyPlayerList);
     }
 
     @Override
-    public List<PlayerInterface> sendToTournamentManager() {
+    public void sendToTournamentManager() throws IOException {
+        Gson gson = new Gson();
         ManagerInterface tournament = new TournamentManager(this.proxyPlayerList);
-        List<PlayerInterface> winners = tournament.runTournament();
-        tournament.getTournamentStatistics().get(PlayerStanding.CHEATER);
-        System.out.println(winners.size());
-        return winners;
+        int winners = tournament.runTournament().size();
+        int cheaters = tournament.getTournamentStatistics().get(PlayerStanding.CHEATER).size();
+        JsonArray output = new JsonArray();
+        output.add(winners);
+        output.add(cheaters);
+        System.out.println(gson.toJson(output));
+        this.server.close();
     }
 
 
