b8e837336075a9f562fdaf08afc638d56ab4d887
Joseph Mullally <jd.mullally@gmail.com>
2020-12-06 16:16:16 -0500

    added client server test file

--- Fish/Common/test/model/ClientServerTest.java
+++ Fish/Common/test/model/ClientServerTest.java
@@ -1,5 +1,32 @@
-package model.testStrategies;
+package model;
+
+import java.io.IOException;
+import model.server.Server;
+import model.server.SignUpServer;
+import org.junit.Test;
 
 public class ClientServerTest {
 
+    private Server server;
+    private int port;
+
+    private void initTournament() throws IOException {
+        this.port = 444444;
+        this.server = new SignUpServer(this.port);
+    }
+
+
+    @Test
+    public void runFullTournamentWithCheater() {
+
+    }
+
+    @Test
+    public void runFullTournament() throws IOException {
+        this.initTournament();
+    }
+
+    @Test void runFullTournamentAllCheaters() {
+
+    }
 }
